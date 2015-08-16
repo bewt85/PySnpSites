ea3e881b72b26c7c39c3a1e1eda7fe16979c4e6e
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-16 20:13:51 +0000

    Tiny bit faster with rstrip
    ```
    (venv)~/PySnpSites$ time ./snp_sites.py random.short.fa random.short.vcf
    
    real    1m36.916s
    user    1m36.450s
    sys     0m0.450s
    ```

--- snp_sites.py
+++ snp_sites.py
@@ -20,25 +20,27 @@ def write_header(sequence_names, reference_length, output_file):
   header_row += sequence_names
   write_row(header_row, output_file)
 
+@profile
 def update_snps(sequence_names, snps, reference_seq, sequence_name, sequence_seq):
   sequence_names.append(sequence_name)
   for i in xrange(len(reference_seq)):
     if reference_seq[i] != sequence_seq[i]:
       snps.setdefault(i, []).append((len(sequence_names)-1, sequence_seq[i]))
 
+@profile
 def parse_fasta(input_fasta):
   for line in input_fasta:
     if line[0] == '>':
       break
-  sequence_name = line[1:].strip()
+  sequence_name = line[1:].rstrip()
   sequence_lines = []
   for line in input_fasta:
     if line[0] == '>':
       yield (sequence_name, "".join(sequence_lines))
-      sequence_name = line[1:].strip()
+      sequence_name = line[1:].rstrip()
       sequence_lines = []
     else:
-      sequence_lines.append(line.strip())
+      sequence_lines.append(line.rstrip())
   yield(sequence_name, "".join(sequence_lines))
 
 if __name__ == '__main__':
