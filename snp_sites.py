4d90b3623ed5860915599514cc9864922253a11c
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-16 19:17:51 +0000

    Custom fasta parser, a bit faster
    ```
    (venv)~/PySnpSites$ time ./snp_sites.py random.short.fa random.short.vcf
    
    real    1m37.270s
    user    1m36.790s
    sys     0m0.430s
    ```

--- snp_sites.py
+++ snp_sites.py
@@ -2,8 +2,10 @@
 
 import argparse
 import json
-from Bio import SeqIO
+import unittest
+
 from collections import OrderedDict
+from cStringIO import StringIO
 
 def write_row(row, output_file):
   output_file.write("\t".join(map(str, row)) + "\n")
@@ -18,12 +20,26 @@ def write_header(sequence_names, reference_length, output_file):
   header_row += sequence_names
   write_row(header_row, output_file)
 
-def update_snps(sequence_names, snps, reference_seq, sequence):
-  sequence_seq = str(sequence.seq)
-  sequence_names.append(sequence.name)
+def update_snps(sequence_names, snps, reference_seq, sequence_name, sequence_seq):
+  sequence_names.append(sequence_name)
   for i in xrange(len(reference_seq)):
     if reference_seq[i] != sequence_seq[i]:
       snps.setdefault(i, []).append((len(sequence_names)-1, sequence_seq[i]))
+
+def parse_fasta(input_fasta):
+  for line in input_fasta:
+    if line[0] == '>':
+      break
+  sequence_name = line[1:].strip()
+  sequence_lines = []
+  for line in input_fasta:
+    if line[0] == '>':
+      yield (sequence_name, "".join(sequence_lines))
+      sequence_name = line[1:].strip()
+      sequence_lines = []
+    else:
+      sequence_lines.append(line.strip())
+  yield(sequence_name, "".join(sequence_lines))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -33,22 +49,21 @@ if __name__ == '__main__':
                       default=open('random.short.fa.vcf', 'w'))
   args = parser.parse_args()
   
-  sequences = SeqIO.parse(args.input, 'fasta')
-  ref = sequences.next()
+  sequences = parse_fasta(args.input)
+  ref_name,ref_seq = sequences.next()
   snps = {}
   sequence_names = []
-  sequence_names.append(ref.name)
+  sequence_names.append(ref_name)
   
-  reference_seq = str(ref.seq)
-  for seq in sequences:
-    update_snps(sequence_names, snps, reference_seq, seq)
+  for seq_name,seq_seq in sequences:
+    update_snps(sequence_names, snps, ref_seq, seq_name, seq_seq)
   
   snps = OrderedDict([(posn, snps[posn]) for posn in sorted(snps.keys())])
   
-  write_header(sequence_names, len(ref.seq), args.output)
+  write_header(sequence_names, len(ref_seq), args.output)
   
   for row_idx, (posn, snp_in_posn) in enumerate(snps.items()):
-    ref_base = str(ref.seq)[posn]
+    ref_base = ref_seq[posn]
     output_row = [1, posn+1, '.', ref_base]
     alts_set = set([seq_base for _,seq_base in snp_in_posn])
     alts = {a: i+1 for i,a in enumerate(alts_set)}
@@ -62,3 +77,15 @@ if __name__ == '__main__':
     indices_at_posn += ['0'] * (len(sequence_names) - len(indices_at_posn))
     output_row += indices_at_posn
     write_row(output_row, args.output)
+
+class TestParseFasta(unittest.TestCase):
+  def test_parse(self):
+    input_fasta = StringIO("""\
+>foo
+AAAAAAAAAAAAAAAAAAA
+>bar
+GGGGGGGGGGGGGGGGGGG
+""")
+    sequences = parse_fasta(input_fasta)
+    self.assertEqual(sequences.next(), ('foo', 'AAAAAAAAAAAAAAAAAAA'))
+    self.assertEqual(sequences.next(), ('bar', 'GGGGGGGGGGGGGGGGGGG'))
