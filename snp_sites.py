2e532d47d255ba143838e768678f5acff6bb02cc
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-16 17:05:50 +0000

    Original snp_sites.py and profile
    ```
    (venv)~/PySnpSites$ time ./snp_sites.py random.really_short.fa
    random.really_short.vcf
    
    real    0m8.836s
    user    0m8.620s
    sys     0m0.210s
    (venv)~/PySnpSites$ time snp-sites -v -o
    random.original.really_short.vcf random.really_short.fa
    
    real    0m0.805s
    user    0m0.710s
    sys     0m0.080s
    (venv)~/PySnpSites$ time ./snp_sites.py random.short.fa
    random.short.vcf
    
    real    3m9.243s
    user    2m53.330s
    sys     0m5.080s
    (venv)~/PySnpSites$ time snp-sites -v -o random.original.short.vcf
    random.short.fa
    
    real    0m16.571s
    user    0m13.630s
    sys     0m1.410s
    ```

--- snp_sites.py
+++ snp_sites.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 from Bio import SeqIO
 from collections import OrderedDict
 
-input_file = open('random.short.fa', 'rU')
-output_file = open('random.short.fa.vcf', 'w')
+def write_row(row, output_file):
+  output_file.write("\t".join(map(str, row)) + "\n")
 
-ref = SeqIO.parse(input_file, 'fasta').next()
-snps = {}
-sequence_names = []
-sequence_names.append(ref.name)
+def write_header(sequence_names, reference_length, output_file):
+  args.output.write("""\
+##fileformat=VCFv4.1
+##contig=<ID=1,length=%i>
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+""" % reference_length)
+  header_row = ["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT"]
+  header_row += sequence_names
+  write_row(header_row, output_file)
 
 def update_snps(sequence_names, snps, reference, sequence):
   for i, (r, s) in enumerate(zip(str(reference.seq), str(sequence.seq))):
@@ -18,19 +24,36 @@ def update_snps(sequence_names, snps, reference, sequence):
       snps.setdefault(i, {})[sequence.name] = s
   sequence_names.append(sequence.name)
 
-for seq in SeqIO.parse(input_file, 'fasta'):
-  update_snps(sequence_names, snps, ref, seq)
-
-snps = OrderedDict([(posn, snps[posn]) for posn in sorted(snps.keys())])
-
-for row_idx, (posn, snp_in_posn) in enumerate(snps.items()):
-  ref_base = str(ref.seq)[posn]
-  output_row = [str(posn), ref_base]
-  alts_set = set(snp_in_posn.values())
-  alts = {a: i+1 for i,a in enumerate(alts_set)}
-  output_row.append(",".join(alts.keys()))
-  alts[ref_base] = 0
-  bases_at_posn = (snp_in_posn.get(name, ref_base) for name in sequence_names)
-  indices_at_posn = [str(alts.get(base, '.')) for base in bases_at_posn]
-  output_row += indices_at_posn
-  output_file.write('\t'.join(output_row) + '\n')
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('input', type=argparse.FileType('r'),
+                      default=open('random.short.fa', 'r'))
+  parser.add_argument('output', type=argparse.FileType('w'),
+                      default=open('random.short.fa.vcf', 'w'))
+  args = parser.parse_args()
+  
+  sequences = SeqIO.parse(args.input, 'fasta')
+  ref = sequences.next()
+  snps = {}
+  sequence_names = []
+  sequence_names.append(ref.name)
+  
+  for seq in sequences:
+    update_snps(sequence_names, snps, ref, seq)
+  
+  snps = OrderedDict([(posn, snps[posn]) for posn in sorted(snps.keys())])
+  
+  write_header(sequence_names, len(ref.seq), args.output)
+  
+  for row_idx, (posn, snp_in_posn) in enumerate(snps.items()):
+    ref_base = str(ref.seq)[posn]
+    output_row = [1, posn+1, '.', ref_base]
+    alts_set = set(snp_in_posn.values())
+    alts = {a: i+1 for i,a in enumerate(alts_set)}
+    output_row.append(",".join(alts.keys()))
+    output_row += ['.', '.', '.', 'GT']
+    alts[ref_base] = 0
+    bases_at_posn = (snp_in_posn.get(name, ref_base) for name in sequence_names)
+    indices_at_posn = [str(alts.get(base, '.')) for base in bases_at_posn]
+    output_row += indices_at_posn
+    write_row(output_row, args.output)
